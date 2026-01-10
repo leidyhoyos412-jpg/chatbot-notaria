3269b19b81070ec089ef774d8939efd00f349e6f
Leidy Hoyos <leidyhoyos412@gmail.com>
2026-01-09 20:26:54 -0500

    add root route

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
+app.get("/", (req, res) => {
+  res.send("🤖 Chatbot Notaría 21 Bogotá activo y funcionando");
+});
+
 app.use(cors());
 app.use(express.json());
 
